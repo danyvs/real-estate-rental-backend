6ab1f6d6409447db81ea35f779c7c60fbd2cbec1
danyvs <daniel.sava99@protonmail.com>
2020-05-07 21:24:14 +0300

    Initial setup

---
+++ src/main/java/com/example/realestaterentalbackend/RealEstateRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.realestaterentalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRentalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateRentalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realestaterentalbackend/RealEstateRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realestaterentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
